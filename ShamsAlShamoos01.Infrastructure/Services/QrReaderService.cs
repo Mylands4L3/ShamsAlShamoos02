6edc869a84d5dc86111a6839ca35ed9246e3f5bb
Mylands4L3 <ghitherly@yahoo.com>
2025-12-09 04:31:15 +0330

    اعمال تغییرات بهینه 140409180431

---
+++ ShamsAlShamoos01.Infrastructure/Services/QrReaderService.cs
@@ -0,0 +1,31 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using ZXing;
+using ZXing.ImageSharp;
+
+namespace ShamsAlShamoos01.Infrastructure.Services
+{
+    public class QrReaderService
+    {
+        public string ReadQrFromFile(string filePath)
+        {
+            if (!System.IO.File.Exists(filePath))
+                return null;
+
+            using var image = Image.Load<Rgba32>(filePath);
+
+            // استفاده از BarcodeReader اختصاصی ImageSharp
+            var reader = new ZXing.ImageSharp.BarcodeReader<Rgba32>
+            {
+                AutoRotate = true,
+                Options = new ZXing.Common.DecodingOptions
+                {
+                    TryHarder = true
+                }
+            };
+
+            var result = reader.Decode(image);
+            return result?.Text;
+        }
+    }
+}
